4a5ed68e72863d4776be746c2d517c48da77a426
deezeesms <deezeesms@gmail.com>
2018-04-02 18:03:39 -0400

    Initial commit

---
+++ src/com/doneezeugo/Main.java
@@ -0,0 +1,10 @@
+package com.doneezeugo;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, hello World 2!");
+        System.out.println("from IDE-IntelliJ 2!");
+    }
+}
+
