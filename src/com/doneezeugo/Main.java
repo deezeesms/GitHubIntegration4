63d7191d31b78aeaebab3e8979eb71baf725dc46
deezeesms <deezeesms@gmail.com>
2018-04-02 20:08:03 -0400

    Update Main.java

--- src/com/doneezeugo/Main.java
+++ src/com/doneezeugo/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, hello World 2!");
         System.out.println("from IDE-IntelliJ 2!");
+         System.out.println("New Input done in Github!");
     }
 }
 
